0720178994374329e4951b911da4cea179061fd4
Ulysse ARNAUD <mexanga@gmail.com>
2020-03-15 20:18:52 +0100

    Cancels redirection when logging

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,7 +15,8 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-             return $this->redirectToRoute('dashboard');
+            return 0;
+            // return $this->redirectToRoute('dashboard');
         }
 
         // get the login error if there is one
